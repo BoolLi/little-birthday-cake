f04285a363694cf882ce1adcd39c0b82d40fb818
boolli <ll024@bucknell.edu>
2015-12-19 01:28:36 -0500

    Add birthday cake

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var sass = require('node-sass-middleware');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -21,6 +22,16 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(
+   sass({
+       src: __dirname + '/sass', 
+       dest: __dirname + '/public/stylesheets',
+       prefix: '/stylesheets',
+       debug: true,       
+   })
+);   
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
