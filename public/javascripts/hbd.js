73c50f292ca3777ec91d6815d566344712cb3bc4
boolli <ll024@bucknell.edu>
2015-12-22 01:33:02 -0500

    Add candle clicking logic

--- public/javascripts/hbd.js
+++ public/javascripts/hbd.js
@@ -1,8 +1,24 @@
 $(function() {
 
-  
-  toggleFlame('.pink-candle');
+  var maxCandleNum = 12;
 
+  $('.candles').click(function(e) {
+  	currentId = parseInt($(this).attr('id'))
+  	previousId = currentId -1 ;
+  	nextId = parseInt($(this).attr('id')) + 1;
+
+  	if (currentId == 1) {
+  		toggleFlame(this);
+  		toggleFlame("#" + (currentId + 1));
+  	} else if (currentId == maxCandleNum) {
+  		toggleFlame(this);
+  		toggleFlame("#" + (currentId - 1));
+  	} else {
+  		toggleFlame(this);
+  		toggleFlame("#" + (currentId + 1));
+  		toggleFlame("#" + (currentId - 1));
+  	}
+  });
 
   function toggleFlame(name) {
   	$(name).toggleClass('no-flame');
